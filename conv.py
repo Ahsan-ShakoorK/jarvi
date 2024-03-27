b8abe9e45da390d99c6a47516f8c544f19d80466
Ahsan Shakoor <79865074+Ahsan-ShakoorK@users.noreply.github.com>
2024-03-27 13:49:23 +0500

    1st

---
+++ conv.py
@@ -0,0 +1,7 @@
+random_text = [
+    "Cool, I'm on it sir"
+    "I'm on it sir"
+    "I'm on it"
+    "just a sec"
+
+]
--- main.py
+++ main.py
@@ -1,16 +1,53 @@
 import pyttsx3
+import speech_recognition as sr
+
+from random import choice
+from decouple import config
 
 engine = pyttsx3.init('sapi5')
 engine.setProperty('volumne', 1.5)
 engine.setProperty('rate', 150)
-
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[1].id)
+
+USER = config('USER')
+host = config('BOT')
 
 def speak(text):
     engine.say(text)
     engine.runAndWait()
 
+def greet():
+    speak(f'Hello {USER}, how can i help you?')
+    print(f'Hello {USER}, how can i help you?')
+
+def take_command():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print('Listening...')
+        r.pause_threshold = 1
+        audio = r.listen(source)
+    try:
+        print('Recognizing...')
+        query = r.recognize_google(audio, language='en-in')
+        print(f'User said: {query}\n')
+        if not 'stop' in query or 'exit' in query:
+            speak('Goodbye')
+            exit()
+    except Exception as e:
+        print('Say that again please...')
+        return 'None'
+    return query
+
+
+
 if __name__ == '__main__':
-    speak('Hi, im Reema. How can i help you?')
-    print('Hi, im Reema. How can i help you?')
+    greet()
+    while True:
+        query = take_command().lower()
+        if host in query:
+            speak(choice(random_text))
+        else:
+            speak('I am sorry, I did not get that')
+            print('I am sorry, I did not get that')
+    
